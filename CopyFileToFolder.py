0ebbf6c7c81cbd28e2433501cb15e6644a19712a
mingweilee97 <snoopylee97@gmail.com>
2021-07-11 14:56:12 +0800

    Add a branch to copy multiple files.

--- CopyFileToFolder.py
+++ CopyFileToFolder.py
@@ -1,30 +1,63 @@
 import shutil
 import os
+import pandas
 
 src="SOURCE OF DIRECTORY WHERE YOU WANT TO SEARCH"              #example: D:\demo
 dest="DESTINATION DIRECTORY WHERE YOU WANT TO COPY THAT FILE"   #example: D:\DemoCopy
-toSearch="FILE TO BE SEARCHED"                                  #example: "Dataset.csv"
-folder=file=0
+search_dataset_file="CSV FILE THAT CONTAINS ALL THE FILENAMES TO COPY"      #example: "Dataset.csv"
+search_success_count=0  # number of file that have been successfully searched
+search_target_count=0   # targeted number of file to be searched
+extension="jpg"         #example: "jpg, png..."
 
 try:
     os.makedirs(dest)
 except:
     print("ALREADY EXISTERD")
-def checkFile(src,searchFile):                                  #Recursive Fuction to Check the file in each Folder
-    global folder,file
-    src_file = os.listdir(src)                                  #listDir returns all files and folder within the src in src_file
-    for file_name in src_file:
-        full_file_name = os.path.join(src, file_name)           #this create a path by joining src and file_name
-        if os.path.isdir(full_file_name):                       #check weather it is a directory or a file
-            folder += 1
-            checkFile(full_file_name,toSearch)                  #Again call the function recursively until it reach the last folder
-        else:
-            if file_name == toSearch:
-                shutil.copy(full_file_name, dest)               #Copy the file to the destination folder
-            file += 1
 
-checkFile(src,toSearch)
-print("FOLDER",folder)                                            #COUNT OF TOTAL FOLDERS
-print("FILE",file)                                                #COUNT OF TOTAL FILES
+#Recursive Fuction to Check the file in each Folder
+def checkFile(src,search_dataset_file,extension):
+    global search_success_count, search_target_count
+    #listDir returns all files and folder within the src in src_file
+    src_file = os.listdir(src)
+
+    # read filename dataset to be searched from csv file
+    data = pandas.read_csv(search_dataset_file)
+    # read only the filename with the title 'id' into dataframe
+    search_id=data["id"]
+     # convert from dataframe to list  
+    search_dataset=search_id.values.tolist()
+    search_target_count=len(search_dataset)
+
+    # loop through filename in the search dataset
+    for search_file_name in search_dataset:
+        # join filename and extension
+        joiner='.'
+        full_search_file_name=joiner.join([str(search_file_name),extension])
+
+        #loop through target file name in the source folder
+        for full_target_file_name in src_file:
+            #this create a path by joining src and file_name
+            full_target_file_name_path = os.path.join(src, full_target_file_name)
+
+            # if target filename equal top search filename
+            if full_target_file_name == full_search_file_name:
+                # Copy the file to the destination folder
+                shutil.copy(full_target_file_name_path, dest)
+                # increment of file count          
+                search_success_count += 1
+
+checkFile(src,search_dataset_file,extension)
+
+#count of targeted searching file
+print("Count of targeted searching file: ", search_target_count)
+
+#COUNT OF TOTAL FILES
+print("Count of file that have been successfully searched",search_success_count)
+
+#compare targeted and final searching count
+if (search_target_count == search_success_count):
+    print("All files have been successfully searched")
+else:
+    print("There are files that have not been successfully searched")
 
 
